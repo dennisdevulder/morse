530ca5f90d9d91abeb5d5165da9ef6a8e823bc66
Dennis de Vulder <dennisdevulder@gmail.com>
2016-06-17 09:10:03 +0000

    trace binary value for 0x7D

--- beep.py
+++ beep.py
@@ -5,7 +5,7 @@ from subprocess import call
 
 pygame.init()
 
-noise = np.random.uniform(-1, 1, 44100*60)
+noise = np.random.uniform(-1, 1, 44100*10)
 
 def setvolume(vol):
   call(["amixer", "cset", "numid=1", vol])
@@ -19,6 +19,7 @@ byt = 0x7D
 mouse_down = False
 send = True 
 setvolume("off")
+payload = []
 
 while True:
   if not playing:
@@ -37,14 +38,17 @@ while True:
     setvolume("400")
 
     if(bytc == 0):
+      payload += [0]
       time.sleep(0.030)
     else:
+      payload += [1]
       time.sleep(0.130)
 
     iteration <<= 1
 
   setvolume("off")
   time.sleep(0.010)
+  print(payload)
 
 sd.wait()
 setvolume("400")
